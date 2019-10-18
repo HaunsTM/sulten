389651ed179959e39c5d22284ef5e03bb1ac77b0
Hauns™ <hans.baldetorp@gmail.com>
2019-10-18 21:37:56 +0200

    Followed a tutorial for setting up a node backend server: https://developer.okta.com/blog/2019/05/07/nodejs-typescript-api
    Basic skeleton were set up with express server.

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,13 +1,18 @@
-import express from 'express';
+import express from "express";
 
+// https://developer.okta.com/blog/2019/05/07/nodejs-typescript-api
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+const port = 8080 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("His!");
 });
-app.listen(port, err => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`server is listening on ${port}`);
+
+app.get("/get", (req, res) => {
+  res.send("His get");
+});
+
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${port}`);
 });
