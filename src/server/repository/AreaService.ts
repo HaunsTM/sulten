7db01ac3099091ccdbe1b890513be168cc1fad52
Hauns™ <hans.baldetorp@gmail.com>
2019-12-05 22:41:13 +0100

    Updated logic for fetching Area[]

--- src/server/repository/AreaService.ts
+++ src/server/repository/AreaService.ts
@@ -2,10 +2,24 @@ import { getConnection } from "typeorm";
 import { Area } from "./entities/Area";
 
 export class AreaService {
+    private readonly AREA_SQL =
+        " SELECT" +
+        "	areas.id as areasId, areas.name as areasName" +
+        " FROM" +
+        "	areas;";
 
     public async getAllAreas(): Promise<Area[]> {
-        const areas: Area[] = await getConnection().getRepository(Area).find();
-        return areas;
+
+        const allAreasResult = await getConnection().query(this.AREA_SQL);
+        const allAreas = allAreasResult.map( (a: any) => {
+            const tempArea = new Area();
+            tempArea.id = a.areasId;
+            tempArea.name = a.areasName;
+
+            return tempArea;
+        });
+
+        return allAreas;
     }
 
 }
