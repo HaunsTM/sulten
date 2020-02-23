d15c022a04e2036357fe7e40e37e866ad12f71c4
Hauns™ <hans.baldetorp@gmail.com>
2020-02-23 17:30:51 +0100

    Toggled longitude <==> latitude

--- database/initialData.sql
+++ database/initialData.sql
@@ -1,10 +1,12 @@
 USE `dbSulten`;
 
 INSERT INTO `areas` (`name`) VALUES ('Lund - Brunnshög') ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
+INSERT INTO `areas` (`name`) VALUES ('Lund - Kungsmarken') ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
 INSERT INTO `areas` (`name`) VALUES ('Lund - Sjukhuset') ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
 INSERT INTO `areas` (`name`) VALUES ('Malmö - Sjukhuset') ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
 INSERT INTO `areas` (`name`) VALUES ('Malmö - Stora Bernstorp') ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
 INSERT INTO `areas` (`name`) VALUES ('Malmö - Västra Hamnen') ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
+INSERT INTO `areas` (`name`) VALUES ('Södra Sandby - Hardeberga') ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
 
 INSERT INTO `labels` (`name`) VALUES ('a_la_carte') ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
 INSERT INTO `labels` (`name`) VALUES ('arabic') ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
@@ -54,80 +56,87 @@ INSERT INTO `weekDays` (`dayIndex`) VALUES (6) ON DUPLICATE KEY UPDATE `id` = LA
 INSERT INTO `prices` (`sek`) VALUES (-1) ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
 
 -- Lund - Brunnshög
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Bricks Eatery", "https://brickseatery.se/lunch/", "55.716502", "13.226820", (SELECT `id` FROM `areas` WHERE `name`= 'Lund - Brunnshög')) 
-    ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);    
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+    ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Scotland Yard", "https://www.fazerfoodco.se/restauranger/restauranger/scotland-yard/", "55.718141", "13.226665", (SELECT `id` FROM `areas` WHERE `name`= 'Lund - Brunnshög')) 
-    ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);    
+    ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
+
+-- Lund - Kungsmarken
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
+	(1, "Matstudio Kungsmarken", "https://www.matstudiokungsmarken.com/lunch", "55.712748", "13.275814", (SELECT `id` FROM `areas` WHERE `name`= 'Lund - Kungsmarken')) 
+    ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
 
 -- Lund - Sjukhuset
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Bistro A Lund - Centralhallen", "https://www.fazer.se/api/location/menurss/current?pageId=28012&language=sv&restaurant=centralhallen", "55.711565", "13.198454", (SELECT `id` FROM `areas` WHERE `name`= 'Lund - Sjukhuset')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Bistro A Lund - Matakuten", "https://www.fazer.se/api/location/menurss/current?pageId=28012&language=sv&restaurant=matakuten", "55.711749", "13.199851", (SELECT `id` FROM `areas` WHERE `name`= 'Lund - Sjukhuset')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Patienthotellet Lund", "https://vardgivare.skane.se/patientadministration/maltider-och-matsedlar/", "55.712325", "13.197028", (SELECT `id` FROM `areas` WHERE `name`= 'Lund - Sjukhuset')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
 
-
 -- Malmö - Sjukhuset
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "FreDa 49", "https://www.freda49.se/lunch-malmo", "55.589817", "12.999629", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Sjukhuset')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Mötesplats CRC", "https://motesplatscrc.se/veckans-lunch/", "55.588726", "12.999257", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Sjukhuset')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
 
 -- Malmö - Stora Bernstorp
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Mor Marnas Matsal", "https://www.mormarnas.se/veckomeny.html", "55.616771", "13.120566", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Stora Bernstorp'))
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Restaurang Öresundsterminalen", "https://www.oresundsterminalen.se/sv/veckomeny", "55.608108", "13.095928", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Stora Bernstorp')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
 
-
-
 -- Malmö - Västra Hamnen
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Glasklart", "https://glasklart.eu/sv/lunch/", "55.615981", "12.990736", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Västra Hamnen')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Lokal 17", "https://lokal17.se/", "55.611998", "12.995554", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Västra Hamnen')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "MH Matsalar", "http://www.mhmatsalar.se/", "55.610916", "12.995886", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Västra Hamnen')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Miamarias", "http://www.miamarias.nu/", "55.613649", "12.991565", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Västra Hamnen')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "P2", "https://www.restaurangp2.se/lunch", "55.614408", "12.988367", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Västra Hamnen')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Restaurang Kolga", "https://restaurangkolga.se/lunch/", "55.612298", "12.998472", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Västra Hamnen')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Restaurang KP", "https://restaurangkp.se/lunchbuffe/", "55.609909", "12.998627", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Västra Hamnen')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Restaurang Niagara", "https://restaurangniagara.se/lunch/", "55.609033", "12.994625", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Västra Hamnen')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Restaurang Variation", "https://www.nyavariation.se/matsedel/", "55.607987", "12.981666", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Västra Hamnen')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Stora Varvsgatan 6", "https://www.storavarvsgatan6.se/projects.html", "55.612268", "12.991715", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Västra Hamnen')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "White Shark", "https://whiteshark.gastrogate.com/lunch/", "55.615005", "12.986016", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Västra Hamnen')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Zen Thai", "http://www.zenthai.se/", "55.614280", "12.989225", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Västra Hamnen')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
-INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `longitude`, `latitude`, `fKAreaId`) VALUES 
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
 	(1, "Årstiderna by the sea", "http://arstidernabythesea.se/lunch/", "55.615184", "12.988667", (SELECT `id` FROM `areas` WHERE `name`= 'Malmö - Västra Hamnen')) 
+    ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
+
+-- Södra Sandby - Hardeberga
+INSERT INTO `restaurants` (`active`, `name`, `menuUrl`, `latitude`, `longitude`, `fKAreaId`) VALUES 
+	(1, "Skrylle Restaurang", "http://www.skryllerestaurang.se/", "55.692211", "13.359271", (SELECT `id` FROM `areas` WHERE `name`= 'Södra Sandby - Hardeberga')) 
     ON DUPLICATE KEY UPDATE `id` = LAST_INSERT_ID(`id`);
